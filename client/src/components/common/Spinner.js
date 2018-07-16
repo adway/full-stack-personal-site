f673946772537c7d773d5c9e18a67e8548a751c0
adwaywadekar <adway@adway.io>
2018-07-16 10:30:20 -0400

    New Spinner

--- client/src/components/common/Spinner.js
+++ client/src/components/common/Spinner.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import spinner from './Spinner.gif';
 
 export default () => {
-	return (
-		<div>
-			<img
-				src={spinner}
-				style={{ width: '200px', margin: 'auto', display: 'block' }}
-				alt="Loading..."
-			/>
-		</div>
-	);
+  return (
+    <div className="individual-project">
+      <div className="sk-cube-grid">
+        <div className="sk-cube sk-cube1" />
+        <div className="sk-cube sk-cube2" />
+        <div className="sk-cube sk-cube3" />
+        <div className="sk-cube sk-cube4" />
+        <div className="sk-cube sk-cube5" />
+        <div className="sk-cube sk-cube6" />
+        <div className="sk-cube sk-cube7" />
+        <div className="sk-cube sk-cube8" />
+        <div className="sk-cube sk-cube9" />
+      </div>
+    </div>
+  );
 };
--- client/src/components/projects/IndividualProject.js
+++ client/src/components/projects/IndividualProject.js
@@ -10,132 +10,132 @@ import Highlight from 'react-highlight';
 import Spinner from '../common/Spinner';
 
 class IndividualProject extends Component {
-	constructor() {
-		super();
-		this.state = {
-			description: ''
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      description: ''
+    };
+  }
 
-	componentDidMount() {
-		if (this.props.match.params.id) {
-			this.props.getProject(this.props.match.params.id);
-		}
-		window.scrollTo(0, 0);
-	}
+  componentDidMount() {
+    if (this.props.match.params.id) {
+      this.props.getProject(this.props.match.params.id);
+    }
+    window.scrollTo(0, 0);
+  }
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.project.project === null && this.props.project.loading) {
-			this.props.history.push('/not-found');
-		}
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.project.project === null && this.props.project.loading) {
+      this.props.history.push('/not-found');
+    }
 
-		const converter = new showdown.Converter({ tables: true });
-		const text = nextProps.project.project.description;
-		const html = converter.makeHtml(text);
-		this.setState({
-			description: html
-		});
-	}
-	render() {
-		const { project, loading } = this.props.project;
-		let content;
-		if (project === null || loading) {
-			content = <Spinner />;
-		} else {
-			content = (
-				<div>
-					{' '}
-					<section className="section">
-						<div className="container">
-							<Link
-								to="/projects"
-								className="button is-link is-outlined"
-								style={{ marginBottom: '2em' }}
-							>
-								Back to Projects
-							</Link>
-							<div className="hero is-medium is-dark">
-								<div className="hero-body">
-									<div className="container">
-										<p className="title" style={{ textAlign: 'center' }}>
-											{this.props.project.project.title}
-										</p>
-										{!isEmpty(this.props.project.project.date) ? (
-											<p className="subtitle" style={{ textAlign: 'center' }}>
-												{moment(this.props.project.project.date).format(
-													'MMMM YYYY'
-												)}
-											</p>
-										) : (
-											''
-										)}
-									</div>
-								</div>
-							</div>
-						</div>
-					</section>
-					<section className="section">
-						<div className="container">
-							<div className="columns">
-								{!isEmpty(this.props.project.project.image) ? (
-									<img
-										src={this.props.project.project.image}
-										className="image"
-										alt={this.props.project.project.title}
-									/>
-								) : (
-									''
-								)}
-							</div>
-						</div>
-					</section>
-					<section className="section">
-						<div className="container">
-							<div className="columns">
-								<div className="column">
-									<div className="markdown-body">
-										<Highlight innerHTML={true}>
-											{this.state.description}
-										</Highlight>
-									</div>
-								</div>
-							</div>
-							<div className="columns">
-								<div className="column">
-									{!isEmpty(this.props.project.project.materials) ? (
-										<p className="standard">
-											<a
-												href={this.props.project.project.materials}
-												target="_blank"
-												rel="noopener noreferrer"
-												className="button is-outlined is-link"
-											>
-												View Materials
-											</a>
-										</p>
-									) : (
-										''
-									)}
-								</div>
-							</div>
-						</div>
-					</section>
-				</div>
-			);
-		}
-		return <div>{content}</div>;
-	}
+    const converter = new showdown.Converter({ tables: true });
+    const text = nextProps.project.project.description;
+    const html = converter.makeHtml(text);
+    this.setState({
+      description: html
+    });
+  }
+  render() {
+    const { project, loading } = this.props.project;
+    let content;
+    if (project === null || loading) {
+      content = <Spinner />;
+    } else {
+      content = (
+        <div>
+          {' '}
+          <section className="section">
+            <div className="container">
+              <Link
+                to="/projects"
+                className="button is-link is-outlined"
+                style={{ marginBottom: '2em' }}
+              >
+                Back to Projects
+              </Link>
+              <div className="hero is-medium is-dark">
+                <div className="hero-body">
+                  <div className="container">
+                    <p className="title" style={{ textAlign: 'center' }}>
+                      {this.props.project.project.title}
+                    </p>
+                    {!isEmpty(this.props.project.project.date) ? (
+                      <p className="subtitle" style={{ textAlign: 'center' }}>
+                        {moment(this.props.project.project.date).format(
+                          'MMMM YYYY'
+                        )}
+                      </p>
+                    ) : (
+                      ''
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="section">
+            <div className="container">
+              <div className="columns">
+                {!isEmpty(this.props.project.project.image) ? (
+                  <img
+                    src={this.props.project.project.image}
+                    className="image"
+                    alt={this.props.project.project.title}
+                  />
+                ) : (
+                  ''
+                )}
+              </div>
+            </div>
+          </section>
+          <section className="section">
+            <div className="container">
+              <div className="columns">
+                <div className="column">
+                  <div className="markdown-body">
+                    <Highlight innerHTML={true}>
+                      {this.state.description}
+                    </Highlight>
+                  </div>
+                </div>
+              </div>
+              <div className="columns">
+                <div className="column">
+                  {!isEmpty(this.props.project.project.materials) ? (
+                    <p className="standard">
+                      <a
+                        href={this.props.project.project.materials}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="button is-outlined is-link"
+                      >
+                        View Materials
+                      </a>
+                    </p>
+                  ) : (
+                    ''
+                  )}
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      );
+    }
+    return <div>{content}</div>;
+  }
 }
 
 IndividualProject.propTypes = {
-	project: PropTypes.object.isRequired,
-	getProject: PropTypes.func.isRequired
+  project: PropTypes.object.isRequired,
+  getProject: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
-	project: state.project
+  project: state.project
 });
 export default connect(
-	mapStateToProps,
-	{ getProject }
+  mapStateToProps,
+  { getProject }
 )(IndividualProject);
