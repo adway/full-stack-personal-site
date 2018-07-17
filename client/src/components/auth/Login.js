7c244aa83ff4dca6c855ca7307996923a235f133
adwaywadekar <adway@adway.io>
2018-07-16 23:13:33 -0400

    Fixed navbar so it resets state on click

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -5,108 +5,108 @@ import { connect } from 'react-redux';
 import { loginUser } from '../../actions/authActions';
 
 class Login extends Component {
-	constructor() {
-		super();
-		this.state = {
-			username: '',
-			password: '',
-			errors: {}
-		};
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+      errors: {}
+    };
 
-		this.onChange = this.onChange.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
-	}
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-	componentDidMount() {
-		if (this.props.auth.isAuthenticated) {
-			this.props.history.push('/dashboard');
-		}
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
 
-		window.scrollTo(0, 0);
-	}
+    window.scrollTo(0, 0);
+  }
 
-	onChange(e) {
-		this.setState({ [e.target.name]: e.target.value });
-	}
+  onChange(e, error) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-	onSubmit(e) {
-		e.preventDefault();
+  onSubmit(e) {
+    e.preventDefault();
 
-		const userData = {
-			username: this.state.username,
-			password: this.state.password
-		};
+    const userData = {
+      username: this.state.username,
+      password: this.state.password
+    };
 
-		this.props.loginUser(userData);
-	}
+    this.props.loginUser(userData);
+  }
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.auth.isAuthenticated) {
-			this.props.history.push('/dashboard');
-		}
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
 
-		if (nextProps.errors) {
-			this.setState({ errors: nextProps.errors });
-		}
-	}
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
 
-	render() {
-		const { errors } = this.state;
-		return (
-			<div>
-				<section className="hero is-dark is-bold is-medium">
-					<div className="hero-body">
-						<div className="container">
-							<h1 className="title is-1">login</h1>
-						</div>
-					</div>
-				</section>
-				<section className="section">
-					<div className="columns">
-						<div className="column">
-							<form noValidate onSubmit={this.onSubmit}>
-								<TextFieldGroup
-									name="username"
-									placeholder="username"
-									value={this.state.username}
-									label="Username"
-									error={errors.username}
-									onChange={this.onChange}
-								/>
+  render() {
+    const { errors } = this.state;
+    return (
+      <div>
+        <section className="hero is-dark is-bold is-medium">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title is-1">login</h1>
+            </div>
+          </div>
+        </section>
+        <section className="section">
+          <div className="columns">
+            <div className="column">
+              <form noValidate onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  name="username"
+                  placeholder="username"
+                  value={this.state.username}
+                  label="Username"
+                  error={errors.username}
+                  onChange={this.onChange}
+                />
 
-								<TextFieldGroup
-									name="password"
-									placeholder="password"
-									value={this.state.password}
-									label="Password"
-									error={errors.password}
-									onChange={this.onChange}
-									type="password"
-								/>
+                <TextFieldGroup
+                  name="password"
+                  placeholder="password"
+                  value={this.state.password}
+                  label="Password"
+                  error={errors.password}
+                  onChange={this.onChange}
+                  type="password"
+                />
 
-								<input type="submit" className="button is-link is-outlined" />
-							</form>
-						</div>
-					</div>
-				</section>
-			</div>
-		);
-	}
+                <input type="submit" className="button is-link is-outlined" />
+              </form>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 Login.propTypes = {
-	loginUser: PropTypes.func.isRequired,
-	auth: PropTypes.object.isRequired,
-	errors: PropTypes.object.isRequired,
-	history: PropTypes.object
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+  history: PropTypes.object
 };
 
 const mapStateToProps = state => ({
-	auth: state.auth,
-	errors: state.errors
+  auth: state.auth,
+  errors: state.errors
 });
 
 export default connect(
-	mapStateToProps,
-	{ loginUser }
+  mapStateToProps,
+  { loginUser }
 )(Login);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,109 +6,122 @@ import { logoutUser } from '../../actions/authActions';
 import classnames from 'classnames';
 
 class Navbar extends Component {
-	constructor() {
-		super();
-		this.state = {
-			navToggled: false
-		};
+  constructor() {
+    super();
+    this.state = {
+      navToggled: false
+    };
 
-		this.onNavbarClick = this.onNavbarClick.bind(this);
-		this.onLogoutClick = this.onLogoutClick.bind(this);
-	}
+    this.onNavbarClick = this.onNavbarClick.bind(this);
+    this.onLogoutClick = this.onLogoutClick.bind(this);
+    this.onLinkClick = this.onLinkClick.bind(this);
+  }
 
-	onNavbarClick() {
-		this.setState(prevState => ({
-			navToggled: !prevState.navToggled
-		}));
-	}
+  onNavbarClick() {
+    this.setState(prevState => ({
+      navToggled: !prevState.navToggled
+    }));
+  }
 
-	onLogoutClick(e) {
-		e.preventDefault();
-		this.props.logoutUser();
-	}
+  onLogoutClick(e) {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
 
-	render() {
-		const { navToggled } = this.state;
+  onLinkClick() {
+    this.setState({ navToggled: false });
+  }
 
-		const { isAuthenticated } = this.props.auth;
+  render() {
+    const { navToggled } = this.state;
 
-		const authLinks = (
-			<div className="navbar-end">
-				<Link to="/" className="navbar-item">
-					home
-				</Link>
-				<Link to="/projects" className="navbar-item">
-					projects
-				</Link>
-				<Link to="/dashboard" className="navbar-item">
-					dashboard
-				</Link>
-				<a href="/" className="navbar-item" onClick={this.onLogoutClick}>
-					logout
-				</a>
-			</div>
-		);
+    const { isAuthenticated } = this.props.auth;
 
-		const guestLinks = (
-			<div className="navbar-end">
-				<Link to="/" className="navbar-item">
-					home
-				</Link>
-				<Link to="/projects" className="navbar-item">
-					projects
-				</Link>
-			</div>
-		);
+    const authLinks = (
+      <div className="navbar-end">
+        <Link to="/" className="navbar-item" onClick={this.onLinkClick}>
+          home
+        </Link>
+        <Link to="/projects" className="navbar-item" onClick={this.onLinkClick}>
+          projects
+        </Link>
+        <Link
+          to="/dashboard"
+          className="navbar-item"
+          onClick={this.onLinkClick}
+        >
+          dashboard
+        </Link>
+        <a
+          href="/"
+          className="navbar-item"
+          onClick={this.onLogoutClick && this.onLinkClick}
+        >
+          logout
+        </a>
+      </div>
+    );
 
-		return (
-			<div>
-				<nav className="navbar is-dark is-fixed-top">
-					<div className="container">
-						<div className="navbar-brand">
-							<a
-								className="navbar-item"
-								href="/"
-								style={{ fontWeight: 'bold' }}
-							>
-								adway s. wadekar
-							</a>
-							<span
-								className={classnames('navbar-burger burger', {
-									'is-active': navToggled
-								})}
-								data-target="navMenu"
-								onClick={this.onNavbarClick}
-							>
-								<span />
-								<span />
-								<span />
-							</span>
-						</div>
-						<div
-							id="navMenu"
-							className={classnames('navbar-menu', {
-								'is-active': navToggled
-							})}
-						>
-							{isAuthenticated ? authLinks : guestLinks}
-						</div>
-					</div>
-				</nav>
-			</div>
-		);
-	}
+    const guestLinks = (
+      <div className="navbar-end">
+        <Link to="/" className="navbar-item" onClick={this.onLinkClick}>
+          home
+        </Link>
+        <Link to="/projects" className="navbar-item" onClick={this.onLinkClick}>
+          projects
+        </Link>
+      </div>
+    );
+
+    return (
+      <div>
+        <nav className="navbar is-dark is-fixed-top">
+          <div className="container">
+            <div className="navbar-brand">
+              <a
+                className="navbar-item"
+                href="/"
+                style={{ fontWeight: 'bold' }}
+              >
+                adway s. wadekar
+              </a>
+              <span
+                className={classnames('navbar-burger burger', {
+                  'is-active': navToggled
+                })}
+                data-target="navMenu"
+                onClick={this.onNavbarClick}
+              >
+                <span />
+                <span />
+                <span />
+              </span>
+            </div>
+            <div
+              id="navMenu"
+              className={classnames('navbar-menu', {
+                'is-active': navToggled
+              })}
+            >
+              {isAuthenticated ? authLinks : guestLinks}
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 Navbar.propTypes = {
-	logoutUser: PropTypes.func.isRequired,
-	auth: PropTypes.object.isRequired
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-	auth: state.auth
+  auth: state.auth
 });
 
 export default connect(
-	mapStateToProps,
-	{ logoutUser }
+  mapStateToProps,
+  { logoutUser }
 )(Navbar);
